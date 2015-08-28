b4bb7f64f05ff66202b5fc8e7fd2856d071cca23
nachoad <elwanax@gmail.com>
2015-08-28 18:09:43 +0200

    added 'id' column

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 ### A data record of each time I fill up the gasoline of my car
 ##### The data I usually note are:
 
-|Name           | Description                          |
-|---------------|--------------------------------------|
-|date		      	|The date I refuel the car             |
-|totKm    	  	|Total of kilometers done with my car  |
-|eLiter   	  	|The price of 1L of gasoline (in €uros)|
-|euros		    	|Final price of the refuel             |
-|liters	    		|The number of liters                  |
-|km           	|Kilometers done from previous refuel  |
+|Names      | Description                             |
+|-----------|-----------------------------------------|
+|id         | Identifier                              |
+|date       | The date I refuel the car               |
+|totKm      | Total of kilometers done with my car    |
+|eLiter     | The price of 1L of gasoline (in Euros)  |
+|euros      | Final price of the refuel               |
+|liters     | The number of liters                    |
+|km         | Kilometers done from previous refuel    |
 
 ###### (gdata.csv file)
 
--- stats_gasData.R
+++ stats_gasData.R
@@ -8,6 +8,9 @@
 # githubcsv <- url("http://raw.githubusercontent.com/nachoad/gasdatarecord/master/gdata.csv")
 # gdata <- read.csv(githubcsv)
 
+# libraries:
+library(ggplot2)
+
 # Take data from local CSV file:
 gdata <- read.csv("gdata.csv")
 
@@ -15,12 +18,26 @@ gdata <- read.csv("gdata.csv")
 str (gdata)
 summary (gdata)
 
-date <- strptime(gdata$date, "%d-%m-%Y")
-date <- as.Date(gdata$date, "%d-%m-%Y")
+# Charts:
+png("./imgs/euros_by_liter_trend.png", width = 800, height = 480, units = "px")
+plot(gdata$eLiter~as.Date(gdata$date, "%d-%m-%Y"), type = "s", xlab="Date",ylab="€/Liter", main="€/liter trend", col='blue')
+dev.off()
 
-plot(gdata$eLiter~as.Date(gdata$date, "%d-%m-%Y"), type = "s", xlab="Date",ylab="€/Liter", main="€/liter trend")
 
-library(ggplot2)
+
+# date <- strptime(gdata$date, "%d-%m-%Y")
+# eliter <- as.integer(gdata$eLiter)
+
+
+ddd <- data.frame(
+      date = as.Date(gdata$date, "%d-%m-%Y"),
+      eliter = gdata$eLiter
+)
+
+ggplot(ddd$date, aes(ddd$date, ddd$eliter)) + geom_line()+
+      scale_x_date(format= "%b-%Y") + xlab("") + ylab("e/litro")
+
+
 ggplot(gdata, aes (x = date)) +
       geom_line(aes(y = gdata$eLiter), color ="blue") +
       xlab("DAY")
